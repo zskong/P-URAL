bf7bb414aaef98a092a44f6e40d984bb48d5212d
zskong <91203521+zskong@users.noreply.github.com>
2026-06-05 19:11:17 +0800

    Update rank_fun_derivative.m

--- funs/rank_fun_derivative.m
+++ funs/rank_fun_derivative.m
@@ -1,22 +1,5 @@
 function y = rank_fun_derivative(x,delta)
 
- %y  = delta*exp(delta^2)./((x+delta).^2+eps);
-%y = delta./(delta+x).^2;
-%  yyy=delta*exp(-x.^2);
-%   yy = yyy.*(1+2*delta.*x+2*(x.^2));
-%  y =yy./((x+delta).^2+eps);
-
-
-%yyy= (4*delta.*x).*(exp(-delta*x.^2));
-%yy = ((exp(-delta*x.^2) + 1).^2+eps);
-
-% %%AAAI
-% yyy=(2*(delta).*exp(-(delta).*x));
-% yy=(exp(-(delta).*x) + 1).^2;
-% y=yyy./yy;
-
-
-%NIPS
 yyy=delta*exp(-x.^2);
    yy = yyy.*(1+2*delta.*x+2*(x.^2));
   y =yy./((x+delta).^2+eps);
